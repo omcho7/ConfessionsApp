820b961ceb97dff9864c47b56f1f3b799ab28443
Aydin324 <ayddin2004@gmail.com>
2025-05-30 17:38:25 +0200

    Added AppDatabase.kt

---
+++ app/src/main/java/com/example/confessionsapp/database/AppDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.confessionsapp.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+/*
+@Database(
+    entities = [
+        User::class,
+        Confession::class,
+        Comment::class,
+        Bookmark::class,
+        Advertisement::class
+    ],
+    version = 1
+)
+
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+    abstract fun confessionDao(): ConfessionDao
+    abstract fun commentDao(): CommentDao
+    abstract fun bookmarkDao(): BookmarkDao
+    abstract fun advertisementDao(): AdvertisementDao
+}
+
+ */
